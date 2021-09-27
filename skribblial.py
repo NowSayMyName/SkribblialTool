02c6b10daa5463c397c48760d16718258898bf59
NowSayMyName <yoann.rouxel@insa-lyon.fr>
2021-09-27 12:24:04 +0200

    added help

--- skribblial.py
+++ skribblial.py
@@ -8,32 +8,29 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='close')
+@bot.command(name='close', brief='Shuts down the bot')
 async def close(ctx):
     await ctx.message.delete()
     await bot.close()
 
-@bot.command(name='add')
+@bot.command(name='add', brief='Adds a new word to the list')
 async def add(ctx, *args):
     await ctx.message.delete()
-    if len(args) < 1:
-        await ctx.send("expected command: !add \"word\" ...")
-    else:
-        word_lst = await get_word_list(await ctx.channel.history(limit=200).flatten())
-        word_lst.extend(x for x in args if x not in word_lst)
-        await ctx.send(create_word_list(word_lst))
+    word_lst = await get_word_list(await ctx.channel.history(limit=200).flatten())
+    word_lst.extend(x for x in args if x not in word_lst)
+    await ctx.send(create_word_list(word_lst))
 
-@bot.command(name='rm')
-async def remove(ctx, *args):
-    await ctx.message.delete()
-    if len(args) < 1:
-        await ctx.send("expected command: !rm \"word\" ...")
-    else:
-        word_lst = await get_word_list(await ctx.channel.history(limit=200).flatten())
-        await ctx.send(create_word_list(word for word in word_lst if word not in args))
+# @bot.command(name='rm')
+# async def remove(ctx, *args):
+#     await ctx.message.delete()
+#     if len(args) < 1:
+#         await ctx.send("expected command: !rm \"word\" ...")
+#     else:
+#         word_lst = await get_word_list(await ctx.channel.history(limit=200).flatten())
+#         await ctx.send(create_word_list(word for word in word_lst if word not in args))
 
 
-@bot.command(name='ng')
+@bot.command(name='ng', brief='Replaces the game link with a new one')
 async def new_game(ctx, arg):
     await ctx.message.delete()
     messages = await ctx.channel.history(limit=200).flatten()
@@ -42,6 +39,14 @@ async def new_game(ctx, arg):
             await discord.Message.delete(msg)
     await ctx.send("Skribblial : " + arg)
 
+@bot.command(name='ping', brief='Pings the funny fellows')
+async def ping(ctx):
+    await ctx.message.delete()
+    messages = await ctx.channel.history(limit=200).flatten()
+    for msg in messages:
+        if "@here" in msg.content:
+            await discord.Message.delete(msg)
+    await ctx.send("@here viendez bande de gens")
 
 async def get_word_list(messages):
     word_lst = []
